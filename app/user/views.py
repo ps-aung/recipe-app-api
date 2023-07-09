890c9a20071fea1cfb1babaa863ec5b63816cd29
pyaesoneaung <pyaesoneag@isgm2.com>
2023-07-09 17:36:49 +0900

    feat: develop user api and authentication

---
+++ app/user/views.py
@@ -0,0 +1,33 @@
+"""
+Views for the user API.
+"""
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import (
+    UserSerializers,
+    AuthTokenSerializer
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializers
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for users"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authentiacted user."""
+    serializer_class = UserSerializers
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
